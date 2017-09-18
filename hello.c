d2ca40637edb3b6dbdb88137f1a033c28708ca0f
forhack <1004xlsrn@gmail.com>
2017-09-18 18:30:36 +0900

    First Commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include "hello.h"
+
+int main() {
+    printf("hello world\n");
+}
+
+char *myStrcpy(char *dest, const char *src) {
+    return NULL;
+}
+
+size_t myStrlen(const char *string) {
+    return 0;
+}
+
+char *myStrcat(char *dest, const char *src) {
+    return NULL;
+}
+
+int myStrcmp(const char *string1, const char *string2) {
+    return 0;
+}
